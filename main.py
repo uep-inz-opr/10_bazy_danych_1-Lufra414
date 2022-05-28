0f2468cd1caa1a052ea154b0dd66025c23c6d5b3
Luiza Frąckowiak <lfrackowiak@tidk.pl>
2022-05-28 18:09:47 +0200

    kolejna

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
                   duration data_type INTEGER , 
                   celltower data_type INTEGER);''')
     
-    polaczania_duze.csv=input()
+    polaczenia_duze.csv=input()
 
     with open('polaczenia_duze.csv','r') as fin: 
       reader = csv.reader(fin, delimiter = ";") # comma is default delimiter
