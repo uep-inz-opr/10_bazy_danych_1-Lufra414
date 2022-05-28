84cac39be2232db8ddb3f0d2503a5986ea336592
Luiza Frąckowiak <lfrackowiak@tidk.pl>
2022-05-28 17:25:00 +0200

    pierwsza

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
+import csv, sqlite3 
 
-if __name__ == "__main__":
-    pass
+
+#To samo co ostatnio, usunieta funkcja mogrify
+class ReportGenerator:
+  def __init__(self,connection, escape_string = "(%s)"):
+    self.connection = connection
+    self.report_text = None
+    self.escape_string = escape_string
+
+  def generate_report(self, user_id):
+    cursor = self.connection.cursor()
+    sql_query = f"Select sum(duration) from polaczenia where from_subscriber ={self.escape_string}"
+    args = (user_id,)
+    cursor.execute(sql_query, args)
+    result = cursor.fetchone()[0]
+    self.report_text = f"Łączny czas trwania dla użytkownika {user_id} to {result}"
+
+  def get_report(self):	
+    return self.report_text
+
+    sqlite_con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES) 
+    cur = sqlite_con.cursor()
+    cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
+                  to_subscriber data_type INTEGER, 
+                  datetime data_type timestamp, 
+                  duration data_type INTEGER , 
+                  celltower data_type INTEGER);''')
+    
+    polaczania_duze_csv=input()
+
+with open('polaczenia_duze.csv','r') as fin: 
+    # csv.DictReader uses first line in file for column headings by default
+    reader = csv.reader(fin, delimiter = ";") # comma is default delimiter
+    next(reader, None)  # skip the headers
+    rows = [x for x in reader]
+    cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows)
+    sqlite_con.commit()
+
+
+    rg = ReportGenerator(connection, escape_string="(%s)")
+    rg.generate_report()
+    print(rg.get_report())
