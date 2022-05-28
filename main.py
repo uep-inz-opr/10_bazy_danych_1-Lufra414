ff98ee07dc4c13335f786ea50bd3089433d919ca
Luiza Frąckowiak <lfrackowiak@tidk.pl>
2022-05-28 19:44:23 +0200

    kolejna

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import sqlite3
+from fileinput import filename
 
 
 
@@ -20,6 +21,7 @@ class ReportGenerator:
     return self.report_text
 
 if __name__ == "__main__":
+    file = input()
     sqlite_con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
     cur = sqlite_con.cursor()
     cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
@@ -27,10 +29,9 @@ if __name__ == "__main__":
                     datetime data_type timestamp, 
                     duration data_type INTEGER , 
                     celltower data_type INTEGER);''') 
-    file = input().replace('(', '').replace(')', '').replace(' ', '').split(",")
+    
     with open(file, 'r') as fin:
       reader = csv.reader(fin, delimiter=",")
-      headers = next(reader)
       next(reader, None)  
       rows = [x for x in reader]
       cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);",  rows)
